42b75078c9cf836cae730a30fe521a14f784e42c
nikitam1337 <nikitam.l@mail.ru>
2023-12-03 15:59:58 +0300

    Task_1 HW_2 compleated

---
+++ HW_2/JJHW_2/src/main/java/org/example/Dog.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Dog extends Animal{
+    String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public void makeSound() {
+        System.out.println("Гав-гав!");
+    }
+
+    public void sit(){  // команда "Сидеть!"
+        System.out.println("Собака села");
+    }
+
+}
