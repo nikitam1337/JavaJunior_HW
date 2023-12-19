c8ce95c812874b93583139749c3bca501129e36a
nikitam1337 <nikitam.l@mail.ru>
2023-12-19 22:17:18 +0300

    step 1

---
+++ HW_4/JJHW_4/src/main/java/org/example/hw4/Program.java
@@ -0,0 +1,17 @@
+package org.example.hw4;
+
+/*
+Создайте базу данных (например, SchoolDB).
+В этой базе данных создайте таблицу Courses с полями id (ключ), title, и duration.
+Настройте Hibernate для работы с вашей базой данных.
+Создайте Java-класс Course, соответствующий таблице Courses, с необходимыми аннотациями Hibernate.
+Используя Hibernate, напишите код для вставки, чтения, обновления и удаления данных в таблице Courses.
+Убедитесь, что каждая операция выполняется в отдельной транзакции.
+ */
+
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
