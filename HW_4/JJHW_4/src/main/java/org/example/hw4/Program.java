f145e018742ed8cc639d98edc124b915d6f75a76
nikitam1337 <nikitam.l@mail.ru>
2023-12-20 20:59:53 +0300

    hw 4 task1 fail

--- HW_4/JJHW_4/src/main/java/org/example/hw4/Program.java
+++ HW_4/JJHW_4/src/main/java/org/example/hw4/Program.java
@@ -10,8 +10,60 @@ package org.example.hw4;
  */
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String url = "jdbc:mysql://localhost:3306/";
+        String user = "root";
+        String password = "password";
+
+        try(Connection connection = DriverManager.getConnection(url, user, password)){
+
+            createDatabase(connection);
+            System.out.println("Database created successfully");
+
+            useDatabase(connection);
+            System.out.println("Use database successfully");
+
+            createTable(connection);
+            System.out.println("Create table successfully");
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
+
+    //region Вспомогательные методы
+
+    private static void createDatabase(Connection connection) throws SQLException {
+        String createDatabaseSQL =  "CREATE DATABASE IF NOT EXISTS SchoolDB;";
+        try (PreparedStatement statement = connection.prepareStatement(createDatabaseSQL)) {
+            statement.execute();
+        }
+    }
+
+    private static void useDatabase(Connection connection) throws SQLException {
+        String useDatabaseSQL =  "USE SchoolDB;";
+        try (PreparedStatement statement = connection.prepareStatement(useDatabaseSQL)) {
+            statement.execute();
+        }
+    }
+
+    private static void createTable(Connection connection) throws SQLException {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS Courses (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), duration VARCHAR(255));";
+        try (PreparedStatement statement = connection.prepareStatement(createTableSQL)) {
+            statement.execute();
+        }
+    }
+
+    //endregion
+
 }
