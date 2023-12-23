57d21c971cecd08819fc6d225f6ed2411ca2b6fc
nikitam1337 <nikitam.l@mail.ru>
2023-12-23 20:16:50 +0300

    hw 4 step 2

--- HW_4/JJHW_4/src/main/java/org/example/models/Course.java
+++ HW_4/JJHW_4/src/main/java/org/example/models/Course.java
@@ -1,5 +1,28 @@
 package org.example.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Courses")
 public class Course {
 
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String duration;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
